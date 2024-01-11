fb847bcd695abbe8f137ac0209bce96c43bc20e2
NILBOGtheSavior <naztechmedia@gmail.com>
2024-01-11 14:45:30 -0600

    OS detect improved

--- js/os_detect.js
+++ js/os_detect.js
@@ -1,23 +1,39 @@
 // Function to detect the user's operating system
 function detectOS() {
-    win_icon = '/img/icons/operating_systems/windows.png';
-    linux_icon = '/img/icons/operating_systems/linux.png';
-    unknown_icon = '/img/icons/operating_systems/unknown.png';
     const userAgent = window.navigator.userAgent;
-    const icon = document.getElementById("os_icon");
     if (userAgent.includes("Windows")) {
-        icon.src = win_icon;
         return "Windows";
     } else if (userAgent.includes("Macintosh") || userAgent.includes("Mac OS")) {
-        icon.src = win_icon;
         return "Mac OS";
     } else if (userAgent.includes("Linux")) {
-        icon.src = linux_icon;
         return "Linux";
     } else {
-        icon.src = unknown_icon;
         return "Unknown";
     }
+  }
+
+  function changeIcon(os_type){
+    const icon = document.getElementById("os_icon");
+    const os = detectOS();
+    windows = '/img/icons/operating_systems/windows.png';
+    linux = '/img/icons/operating_systems/linux.png';
+    unknown = '/img/icons/operating_systems/unknown.png';
+
+    switch (os) {
+        case "Windows":
+            icon.src = windows;
+            break;
+        case "Mac OS":
+            icon.src = unknown;
+            break;
+        case "Linux":
+            icon.src = linux;
+            break;
+        default:
+            icon.src = unknown;
+      }
+
+    
   }
 
   // Function to handle file downloads based on OS
@@ -42,4 +58,5 @@ function detectOS() {
 
   document.addEventListener("DOMContentLoaded", function() {
     detectOS();
+    changeIcon();
  });
